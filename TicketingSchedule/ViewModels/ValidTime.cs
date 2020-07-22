328f134654c9adaadc6a2e895cf4ed5fd6369426
Edo Novanto <novantoedo@gmail.com>
2020-07-22 15:24:50 +0700

    Client-side Validation

---
+++ TicketingSchedule/ViewModels/ValidTime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace TicketingSchedule.ViewModels
+{
+    public class ValidTime : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime date;
+
+            var isValid = DateTime.TryParseExact(Convert.ToString(value),
+                "HH:mm", 
+                CultureInfo.CurrentCulture,
+                DateTimeStyles.None, 
+                out date);
+
+
+
+            return (isValid);
+        }
+    }
+}
