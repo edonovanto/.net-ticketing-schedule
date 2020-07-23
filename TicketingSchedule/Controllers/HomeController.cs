b261cb742062dcf600813d5acd5ef7f37ea30ce5
Edo Novanto <novantoedo@gmail.com>
2020-07-23 19:37:23 +0700

    Modify homepage

--- TicketingSchedule/Controllers/HomeController.cs
+++ TicketingSchedule/Controllers/HomeController.cs
@@ -1,16 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using TicketingSchedule.Models;
 
 namespace TicketingSchedule.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            var upcomingGigs = _context.Gig
+                .Include(g => g.Artis)
+                .Where(g => g.DateTime > DateTime.Now);
+
+            return View(upcomingGigs);
         }
 
         public ActionResult About()
